8968b318c4dac32636e4f30699942fc1cf260c59
AaronZzzzzz <43912615+AaronZzzzzz@users.noreply.github.com>
2021-10-29 09:38:10 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
-sudo apt-get install apt-transport-https ca-certificates dirmngr
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv E0C56BD4
-echo "deb https://repo.clickhouse.com/deb/stable/ main/" | sudo tee \
-    /etc/apt/sources.list.d/clickhouse.list
-sudo apt-get update
-sudo apt-get install -y clickhouse-server clickhouse-client
+#sudo apt-get install apt-transport-https ca-certificates dirmngr
+#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv E0C56BD4
+#echo "deb https://repo.clickhouse.com/deb/stable/ main/" | sudo tee \
+#    /etc/apt/sources.list.d/clickhouse.list
+#sudo apt-get update
+#sudo apt-get install -y clickhouse-server clickhouse-client
+#
+#sudo apt-get install awscli
+#
+#sudo cp merge-s3.xml /etc/clickhouse-server/config.d/
 
-sudo apt-get install awscli
-sudo apt-get install gh
-
-sudo cp ./aws_setup/merge-s3.xml /etc/clickhouse-server/config.d/
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install gh
