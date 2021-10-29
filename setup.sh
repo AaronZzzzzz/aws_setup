7a0a13f5bdabcc37ae3773dedfea3786e1f94ade
AaronZzzzzz <43912615+AaronZzzzzz@users.noreply.github.com>
2021-10-29 10:33:10 -0400

    update

--- setup.sh
+++ setup.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 
-#sudo apt-get install apt-transport-https ca-certificates dirmngr
-#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv E0C56BD4
-#echo "deb https://repo.clickhouse.com/deb/stable/ main/" | sudo tee \
-#    /etc/apt/sources.list.d/clickhouse.list
-#sudo apt-get update
-#sudo apt-get install -y clickhouse-server clickhouse-client
-#
-#sudo apt-get install awscli
-#echo -e "\n \n us-east-2 json" | aws configure
-#
-#
-#curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
-#echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-#sudo apt update
-#sudo apt install gh
+sudo apt-get install apt-transport-https ca-certificates dirmngr
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv E0C56BD4
+echo "deb https://repo.clickhouse.com/deb/stable/ main/" | sudo tee \
+    /etc/apt/sources.list.d/clickhouse.list
+sudo apt-get update
+sudo apt-get install -y clickhouse-server clickhouse-client
+
+sudo apt-get install awscli
+echo -e "\n \n us-east-2 json" | aws configure
+
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install gh
 
 sudo cp merge-s3.xml /etc/clickhouse-server/config.d/merge-s3.xml
 sudo cp config.xml /etc/clickhouse-server/config.xml
