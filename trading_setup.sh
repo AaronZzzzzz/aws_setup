cd1dc406fc39d5af67cacf94888521cfd4d62c3e
AaronZzzzzz <43912615+AaronZzzzzz@users.noreply.github.com>
2021-10-30 18:56:29 -0400

    Create trading_setup.sh

---
+++ trading_setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install -y gh
+
+sudo apt install python3-pip
+pip install pandas
+pip install ccxt
+
